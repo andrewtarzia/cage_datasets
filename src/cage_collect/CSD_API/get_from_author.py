05599804494a9ed224204bcffaaaa2a1cf1c2b21
Andrew <andrew.tarzia@gmail.com>
2019-03-01 17:23:36 +0000

    modified script to check for other properties and output a list of REFCODES to load into Conquest

--- src/cage_collect/CSD_API/get_from_author.py
+++ src/cage_collect/CSD_API/get_from_author.py
@@ -14,7 +14,7 @@ from ccdc.io import EntryReader, CrystalWriter
 from ccdc.search import TextNumericSearch
 
 
-def write_entry(number, DOI, CSD, solvent, disorder):
+def write_entry(author, number, DOI, CSD, solvent, disorder):
     '''Write entry to CIF DB file that contains all names and references for a
     structure.
 
@@ -23,7 +23,15 @@ def write_entry(number, DOI, CSD, solvent, disorder):
         f.write(author+','+number+','+DOI+','+CSD+','+solvent+','+disorder+'\n')
 
 
-author_file = 'CCDC_code.txt'
+def write_REFCODES(CSD):
+    '''Write REFCODE to file.
+
+    '''
+    with open('DB.gcd', 'a') as f:
+        f.write(CSD+'\n')
+
+
+author_file = 'author_list.txt'
 # files = []
 authors = []
 DOIs = []
@@ -34,28 +42,42 @@ for line in open(author_file, 'r'):
 with open('CIF_DB_author.txt', 'w') as f:
     f.write('author,number,DOI,CSD,solvent,disorder\n')
 
+with open('DB.gcd', 'w') as f:
+    f.write('')
+
 count = 0
 count_no = 0
 idents = []
-for i, number in enumerate(authors):
+for i, author in enumerate(authors):
     count_no += 1
-    # print(number)
     query = TextNumericSearch()
-    query.add_ccdc_number(int(number))
+    query.add_author(author)
     hits = query.search(database='CSD')
     if len(hits) == 0:
-        print(number)
+        print(author)
     for hit in hits:
-        # print('%s %s' % (hit.identifier, hit.entry.ccdc_number))
-        # print(hit.entry.chemical_name)
-        # print(hit.entry.is_polymeric)
+        print('%s %s' % (hit.identifier, hit.entry.ccdc_number))
+        print(hit.entry.chemical_name)
+        print(hit.entry.is_polymeric)
         # skip polymeric structures
+        if hit.entry.chemical_name is not None:
+            if 'catena' in hit.entry.chemical_name:
+                continue
         if hit.entry.is_polymeric is True:
             continue
+        # skip if structure is powder study
+        if hit.entry.is_powder_study is True:
+            continue
+        # skip structures that are purely organic
+        if hit.entry.is_organometallic is False:
+            continue
+        print('passed!')
         # note structures with solvent
         solvent = 'n'
-        if len(hit.entry.chemical_name.split(' ')) > 1:
-            solvent = 'y'
+        if hit.entry.chemical_name is not None:
+            if len(hit.entry.chemical_name.split(' ')) > 1:
+                solvent = 'y'
+        # note structures with disorder
         disorder = 'n'
         if hit.entry.has_disorder is True:
             disorder = 'y'
@@ -63,10 +85,11 @@ for i, number in enumerate(authors):
         # write to CIF
         if hit.identifier not in idents:
             idents.append(hit.identifier)
-            CrystalWriter(hit.identifier+'.cif').write(crystal.disordered_molecule)
-            write_entry(author, hit.entry.ccdc_number,
+            # CrystalWriter(hit.identifier+'.cif').write(crystal)
+            write_entry(author, str(hit.entry.ccdc_number),
                         hit.entry.doi, hit.identifier, solvent,
                         disorder)
+            write_REFCODES(hit.identifier)
             count += 1
 
 print(count, 'cifs found from', count_no, 'authors')
